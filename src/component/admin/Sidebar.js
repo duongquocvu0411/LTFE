0727e846c68c099d9d60e7c0403fe8758d7a8415
thai-ITa <nguyenquocthai30012004@gmail.com>
2024-09-10 13:40:28 +0700

    da fix tieude

--- src/component/admin/Sidebar.js
+++ src/component/admin/Sidebar.js
@@ -14,7 +14,7 @@ const Sidebar = () => {
         className={`d-flex flex-column bg-dark ${isCollapsed ? 'collapsed' : ''}`} 
         style={{
           width: isCollapsed ? '80px' : '250px', 
-          height: '89vh',
+          height: '86.5vh',
           overflowY: 'auto',  // Enable vertical scrolling
           overflowX: 'hidden', // Disable horizontal scroll
           transition: 'width 0.3s',
@@ -22,7 +22,7 @@ const Sidebar = () => {
         }}
       >
         {/* Sidebar Header */}
-        <div className="sidebar-header p-4 text-white d-flex justify-content-between align-items-center">
+        <div className="sidebar-header p-4 text-white d-flex justify-content-between align-items-cente r">
           <h4 className={`m-0 ${isCollapsed ? 'd-none' : ''}`}>AdminLTE</h4>
           <button className="btn btn-dark" onClick={handleToggle}>
             <i className={`fas ${isCollapsed ? 'fa-chevron-right' : 'fa-chevron-left'}`}></i>
@@ -32,24 +32,24 @@ const Sidebar = () => {
         {/* Sidebar Menu */}
         <ul className="nav flex-column list-unstyled components" style={{ marginRight: '-10px' }}>
           <li className="nav-item p-3">
-            <Link to="/" className="nav-link text-white d-flex align-items-center">
+            <Link to="/admin/Dashboard" className="nav-link text-white d-flex align-items-center">
               <i className="fas fa-home"></i>
               <span className={`ms-3 ${isCollapsed ? 'd-none' : ''}`}>Home</span>
             </Link>
           </li>
           <li className="nav-item p-3">
-            <Link to="/profile" className="nav-link text-white d-flex align-items-center">
+            <Link to="/admin/profile" className="nav-link text-white d-flex align-items-center">
               <i className="fas fa-user"></i>
               <span className={`ms-3 ${isCollapsed ? 'd-none' : ''}`}>Profile</span>
             </Link>
           </li>   <li className="nav-item p-3">
-            <Link to="/profile2" className="nav-link text-white d-flex align-items-center">
+            <Link to="/admin/profile2" className="nav-link text-white d-flex align-items-center">
               <i className="fas fa-user"></i>
               <span className={`ms-3 ${isCollapsed ? 'd-none' : ''}`}>Profile2</span>
             </Link>
           </li> 
             <li className="nav-item p-3">
-            <Link to="/profile3" className="nav-link text-white d-flex align-items-center">
+            <Link to="/admin/profile3" className="nav-link text-white d-flex align-items-center">
               <i className="fas fa-user"></i>
               <span className={`ms-3 ${isCollapsed ? 'd-none' : ''}`}>Profile3</span>
             </Link>
