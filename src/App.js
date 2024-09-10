5837e754e5292f834c099116e1a016f664133013
thai-ITa <nguyenquocthai30012004@gmail.com>
2024-09-10 12:53:32 +0700

    da fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Sidebar from "./component/admin/Sidebar";
+
 import Dashboard from "./component/admin/page/Dashboard";
 import Profile from "./component/admin/page/Profile";
-import Header from "./component/admin/Header";
-import Footer from './component/admin/Footer';
+
 import Profile2 from "./component/admin/page/Profile2";
 import Profile3 from './component/admin/page/Profile3';
 import HeadersUsers from './component/users/HeaderUsers';
 
+import Shop from "./component/users/Shop";
+import Checkout from "./component/users/Checkout";
+
 function App() {
+  
   return (
+     
+
     <>
       {/* Wrap your app with BrowserRouter */}
       <Router>
@@ -19,7 +24,9 @@ function App() {
           <Route path="/admin/profile" element={<Profile />} />
           <Route path="/admin/profile2" element={<Profile2 />} />
           <Route path="/admin/profile3" element={<Profile3 />} />
-          <Route path="/" element={<HeadersUsers/>}/>
+          <Route path="/" element={<HeadersUsers />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/checkout" element={<Checkout />} />
         </Routes>
       </Router>
     </>
